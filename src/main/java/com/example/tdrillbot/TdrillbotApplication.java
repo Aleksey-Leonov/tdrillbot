8d61b591bdac70947bfe88008104b755aae00c86
aleksejleonov <typical_drilling@mail.ru>
2022-06-21 10:48:48 +0500

    Initial commit

---
+++ src/main/java/com/example/tdrillbot/TdrillbotApplication.java
@@ -0,0 +1,13 @@
+package com.example.tdrillbot;
+
+import org.springframework.boot.SpringApplication;
+import org.springframework.boot.autoconfigure.SpringBootApplication;
+
+@SpringBootApplication
+public class TdrillbotApplication {
+
+	public static void main(String[] args) {
+		SpringApplication.run(TdrillbotApplication.class, args);
+	}
+
+}
